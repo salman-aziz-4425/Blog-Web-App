7d44cdf126340f19e36753411f8e5e337d773138
salman-aziz-4425 <salmanaziz216@gmail.com>
2023-07-14 16:29:25 +0500

    Today commits

---
+++ src/Components.js/Navbar.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUsers, addAllData } from '../redux/postSlicer';
+
+export default function Navbar() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const allData = useSelector((state) => state.postHandler.allData);
+
+  return (
+    <Box sx={{ flexGrow: 1, marginBottom: 5 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, fontFamily: 'Arial', fontSize: '1.5rem' }}
+          >
+            Post Manager
+          </Typography>
+          <Button
+            color="inherit"
+            onClick={() => {
+              localStorage.setItem('ActiveUser', 0);
+              localStorage.setItem('usersData', JSON.stringify(allData));
+              dispatch(addUsers(0));
+              dispatch(addAllData([]));
+              navigate('/');
+            }}
+            sx={{ fontFamily: 'Arial', fontSize: '1rem', fontWeight: 'bold' }}
+          >
+            Logout
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
