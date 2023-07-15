879c7416149998ae1bda02bda32ff44bebc6e11b
salman-aziz-4425 <salmanaziz216@gmail.com>
2023-07-15 18:29:23 +0500

    feat:implemented post feature

--- src/redux/postSlicer.js
+++ src/redux/postSlicer.js
@@ -14,9 +14,47 @@ export const postSlice = createSlice({
     addAllData: (state, action) => {
       state.allData = [...action.payload];
     },
+    handleAddPostState: (state, action) => {
+      const { userId, newPostTitle, newPostContent } = action.payload;
+      if (newPostTitle.trim() !== "" && newPostContent.trim() !== "") {
+        let newPost = {
+          userId: parseInt(userId),
+          id: state.allData[userId - 1].posts[state.allData[userId - 1].posts.length - 1].id + 1,
+          title: newPostTitle,
+          body: newPostContent,
+          comments: [],
+        };
+        const updatedPosts = [...state.allData];
+        updatedPosts[userId - 1].posts.unshift(newPost);
+        state.allData = updatedPosts;
+        localStorage.setItem('usersData',JSON.stringify(updatedPosts))
+      }
+    },
+    handleDeletePostState: (state, action) => {
+      const { userId, postId } = action.payload;
+      let updatedPosts = state.allData.slice();
+      state.allData[userId - 1].posts = updatedPosts[userId - 1].posts.filter((post)=>{
+        return post.id !== postId
+      })
+      localStorage.setItem('usersData',JSON.stringify(state.allData))
+    },
+  handleUpdatePostState: (state, action) => {
+    const updatedPosts = [...state.allData];
+    let postPostion=updatedPosts[action.payload.userIndex-1]?.posts.findIndex((item)=>item?.id===action.payload.postIndex)
+    let post=updatedPosts[action.payload.userIndex-1].posts[postPostion]
+    if(action.payload.postName.trim()!==""){
+      post.title=action.payload.postName
+    }
+    if(action.payload.postContent.trim()!==""){
+      post.body=action.payload.postContent
+    }
+    updatedPosts[action.payload.userIndex-1].posts[postPostion]=post
+    localStorage.setItem('usersData',JSON.stringify(updatedPosts))
+    alert("Post Updated Successfully")
+  },
 }
 });
 
-export const { addUsers,addAllData } = postSlice.actions;
+export const { addUsers,addAllData,handleDeletePostState,handleUpdatePostState,handleAddPostState,handlePostCommentState } = postSlice.actions;
 
 export default postSlice.reducer;
