7747a69585f112155182e6a2195625af517bea55
salman-aziz-4425 <salmanaziz216@gmail.com>
2023-07-15 17:41:05 +0500

    feats:project setup

--- src/App.js
+++ src/App.js
@@ -4,31 +4,22 @@ import Home from './Pages/Home';
 import Login from './Pages/Login';
 import { useSelector } from 'react-redux';
 const ProtectedRoute = ({ userStatus, children }) => {
-  if (userStatus===0) {
-    return <Navigate to="/" replace/>;
-  }
 
   return  children ;
 };
 
 const OpeningProtectedRoute = ({ userStatus, children }) => {
-  if (userStatus!==0) {
-    return <Navigate to="/Home" replace/>;
-  }
-
   return  children ;
 };
 
 const App = () => {
-  const userStatus = useSelector((state) => state.postHandler.users) || 0;
-  console.log(userStatus)
   return (
     <Router>
       <Routes>
         <Route
           exact path="/"
           element={
-            <OpeningProtectedRoute userStatus={userStatus}>
+            <OpeningProtectedRoute >
                <Login />
             </OpeningProtectedRoute>
           }
@@ -36,7 +27,7 @@ const App = () => {
         <Route
           path="Home"
           element={
-            <ProtectedRoute userStatus={userStatus}>
+            <ProtectedRoute >
               <Home />
             </ProtectedRoute>
           }
