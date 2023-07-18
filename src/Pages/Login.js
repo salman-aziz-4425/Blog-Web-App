ed94bfb9a598f5cd6fb259d1580e8d5c42982898
salman-aziz-4425 <salmanaziz216@gmail.com>
2023-07-18 14:34:28 +0500

    feat:implemented comments feature

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -7,8 +7,46 @@ import { useDispatch } from 'react-redux';
 import { addAllData, addUsers } from '../redux/postSlicer';
 import 'react-toastify/dist/ReactToastify.css';
 import toast from 'react-hot-toast';
-import {Container,Form,Input,ButtonWrapper,Heading,LoginLink} from './styles/loginandsignup';
 
+
+const Container = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100vh',
+  backgroundColor: '#f9f9f9',
+});
+
+const Form = styled('form')({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '400px',
+  padding: '32px',
+  borderRadius: '4px',
+  backgroundColor: '#ffffff',
+});
+
+const Input = styled(TextField)({
+  marginBottom: '16px',
+});
+
+const ButtonWrapper = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+const Heading = styled(Typography)({
+  fontSize: '2rem',
+  fontWeight: 'bold',
+  marginBottom: '24px',
+});
+
+const LoginLink = styled(Typography)({
+  fontSize: '0.875rem',
+  marginTop: '16px',
+});
 
 
 function SignupPage() {
