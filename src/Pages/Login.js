7f5607e34a72cf1c0712004ead970e684d70d448
salman-aziz-4425 <salmanaziz216@gmail.com>
2023-07-18 14:43:02 +0500

    docs:code refactored

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,11 +1,15 @@
+
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {  Button,} from '@mui/material';
-import {Container,Form,Input,ButtonWrapper,Heading,LoginLink} from './styles/loginandsignup'
+import { Box, Typography, TextField, Button, Link } from '@mui/material';
+import { styled } from '@mui/system';
 import { useDispatch } from 'react-redux';
 import { addAllData, addUsers } from '../redux/postSlicer';
 import 'react-toastify/dist/ReactToastify.css';
 import toast from 'react-hot-toast';
+import {Container,Form,Input,ButtonWrapper,Heading,LoginLink} from "../Components/StyledComponents"
+
+
 
 function SignupPage() {
   const [userData, setUserData] = useState({
@@ -15,7 +19,7 @@ function SignupPage() {
     password: '',
     posts: [],
   });
-  const [flag, setFlag] = useState(true);
+  const [loginSignupOption, setloginSignupOption] = useState(true);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -25,8 +29,7 @@ function SignupPage() {
       [e.target.name]: e.target.value,
     });
   };
-  const notify = () => toast('Here is your toast.');
-  const submitForm = (e) => {
+  const submitForm = async(e) => {
     e.preventDefault();
     let allData = localStorage.getItem('usersData') || [];
     let userCount = parseInt(localStorage.getItem('userCount')) || 0;
@@ -35,7 +38,7 @@ function SignupPage() {
       allData = JSON.parse(allData);
     }
 
-    if (flag === false) {
+    if (loginSignupOption === false) {
       let index = allData.findIndex((item) => item?.email === userData?.email);
       if (index !== -1) {
         alert('User already exists');
@@ -53,102 +56,112 @@ function SignupPage() {
         alert('iNVALID EMAIL');
         return;
       }
-
-
-
       userCount++;
-      fetch('https://jsonplaceholder.typicode.com/posts?userId=' + userCount)
-        .then((res) => res.json())
-        .then((data) => {
-          userData.posts = data;
-          userData.userId = userCount;
-          allData.push(userData);
-          localStorage.setItem('usersData', JSON.stringify(allData));
-          dispatch(addUsers(allData[index]?.userId));
-          dispatch(addAllData(allData));
-          localStorage.setItem('userCount', userCount);
-          alert('User created successfully');
-          toast.success('User created successfully');
-          return navigate('/');
-        });
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userCount}`);
+      const data = await res.json();
+      userData.posts = data;
+      userData.userId = userCount;
+      allData.push(userData);
+      localStorage.setItem('usersData', JSON.stringify(allData));
+      dispatch(addUsers(allData[index]?.userId));
+      dispatch(addAllData(allData));
+      localStorage.setItem('userCount', userCount);
+      alert('User created successfully');
+      return navigate('/');
     } else {
       if (allData.length === 0) {
         alert('User does not exist');
         return;
       } else {
-        let index = allData.findIndex((item) => item.email === userData.email&&item.password===userData.password);
-        if (index !== -1) {
-          toast.success('User logged in successfully');
-          toast('User logged in successfully');
-          dispatch(addUsers(allData[index].userId));
-          dispatch(addAllData(allData));
-          localStorage.setItem('ActiveUser', allData[index].userId);
-          navigate('/Home');
-        } else {
-          alert('Invalid credentials');
+        try{
+          let index = allData.findIndex((item) => item.email === userData.email&&item.password===userData.password);
+          if (index !== -1) {
+            toast.success('User logged in successfully');
+            toast('User logged in successfully');
+            dispatch(addUsers(allData[index].userId));
+            dispatch(addAllData(allData));
+            if (typeof localStorage !== 'undefined') {
+              localStorage.setItem('ActiveUser', allData[index].userId);
+              navigate('/Home');
+            } else {
+              throw new Error('localStorage is not defined');
+            }
+          } else {
+            alert('Invalid credentials');
+          }
+        }catch(err){
+          alert(err);
         }
       }
     }
   };
 
+  const inputProps = [
+    {
+      visibility: loginSignupOption ? 'hidden' : 'visible',
+      name: 'name',
+      type: 'text',
+      label: 'Name',
+      placeholder: 'Name',
+      variant: 'outlined',
+      value: userData.name,
+    },
+    {
+      visibility:'visible',
+      name: 'email',
+      type: 'email',
+      label: 'Email',
+      placeholder: 'Email',
+      variant: 'outlined',
+      value: userData.email,
+    },
+    {
+      visibility:'visible',
+      name: 'password',
+      type: 'password',
+      label: 'Password',
+      placeholder: 'Password',
+      variant: 'outlined',
+      value: userData.password,
+    }
+  ]
+
   return (
     <Container>
-      <Heading>{flag === false ? 'Sign Up' : 'Sign In'}</Heading>
+      <Heading>{loginSignupOption === false ? 'Sign Up' : 'Sign In'}</Heading>
       <Form>
-        {!flag && (
-          <Input
-            name="name"
-            type="text"
-            label="Name"
-            placeholder="Name"
-            variant="outlined"
-            value={userData.name}
+        {
+          inputProps.map((item,index)=>(
+            item.visibility==='visible'&&<Input
+            key={index}
+            name={item.name}
+            type={item.type}
+            label={item.label}
+            placeholder={item.placeholder}
+            variant={item.variant}
+            value={item.value}
             onChange={handleChange}
           />
-        )}
-        <Input
-          name="email"
-          type="email"
-          label="Email"
-          placeholder="Email"
-          variant="outlined"
-          value={userData.email}
-          onChange={handleChange}
-        />
-        <Input
-          name="password"
-          type="password"
-          label="Password"
-          placeholder="Password"
-          variant="outlined"
-          value={userData.password}
-          onChange={handleChange}
-        />
-        {flag === false ? (
+          ))
+        }
           <ButtonWrapper>
             <Button variant="contained" color="primary" onClick={submitForm}>
-              Sign Up
+              {
+                 !loginSignupOption?'Sign Up':'Sign In'
+              }
             </Button>
             <LoginLink variant="body2">
-              Already have an account?{' '}
-              <Button onClick={() => setFlag(!flag)} color="primary">
-                Sign In
+              {
+                !loginSignupOption?'Already have an account?':'Dont have a account'
+              }
+              <Button onClick={() =>  setloginSignupOption(!loginSignupOption)} color="primary">
+                {
+                  !loginSignupOption?'Sign In':'Sign Up'
+                }
+               
               </Button>
             </LoginLink>
           </ButtonWrapper>
-        ) : (
-          <ButtonWrapper>
-            <Button variant="contained" color="primary" onClick={submitForm}>
-              Sign In
-            </Button>
-            <LoginLink variant="body2">
-              Don't have an account?{' '}
-              <Button onClick={() => setFlag(!flag)} color="primary">
-                Sign Up
-              </Button>
-            </LoginLink>
-          </ButtonWrapper>
-        )}
       </Form>
     </Container>
   );
